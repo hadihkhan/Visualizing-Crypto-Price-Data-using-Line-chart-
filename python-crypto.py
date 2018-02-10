0e2a63f4e0d0869efad100626ce91f27d09e11cc
Hadi Hussain Khan <hadihussainkhan@Hadis-MacBook-Pro.local>
2018-02-11 02:20:10 +0500

    Assignment 1 - OOP implementation for Any Coin [histoday]

--- python-crypto.py
+++ python-crypto.py
@@ -4,28 +4,44 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def daily_price_historical(symbol, comparison_symbol, all_data=False, limit=2000, aggregate=1, exchange=''):
-    url = 'https://min-api.cryptocompare.com/data/histominute?fsym={}&tsym={}&limit={}&aggregate={}'\
-            .format(symbol.upper(), comparison_symbol.upper(), limit, aggregate)
-    if exchange:
-        url += '&e={}'.format(exchange)
-    if all_data:
-        url += '&allData=true'
-    page = requests.get(url)
-    data = page.json()['Data']
-    df = pd.DataFrame(data)
-    df['timestamp'] = [datetime.datetime.fromtimestamp(d) for d in df.time]
-    return df
+class cryptoVisual:
+    def __init__(self, sym, comp_sym, all_data=False, limit=1000, aggregate=1, exchange=''):
+        self.symbol = sym
+        self.comp_sym = comp_sym
+        self.all_data = all_data
+        self.limit = limit
+        self.aggregate = aggregate
+        self.exchange = exchange
+        self.url = 'https://min-api.cryptocompare.com/data/histoday?fsym={}&tsym={}&limit={}&aggregate={}' \
+            .format(self.symbol.upper(), self.comp_sym.upper(), self.limit, self.aggregate)
+        if self.exchange:
+            self.url += '&e={}'.format(self.exchange)
+        if self.all_data:
+            self.url += '&allData=true'
+
+    def getData(self):
+        page = requests.get(self.url)
+        data = page.json()['Data']
+        dataFrame = pd.DataFrame(data)
+        dataFrame['timestamp'] = [datetime.datetime.fromtimestamp(d) for d in dataFrame.time]
+        return dataFrame
+
+    def plotGraph(self):
+        data = self.getData()
+        fg = plt.figure(figsize=(20, 10))
+        plt.plot(data.timestamp, data.high)
+        plt.title(self.symbol + ' To ' + self.comp_sym, fontsize=24)
+        plt.ylabel('Price In ' + self.comp_sym, fontsize=18)
+        plt.xlabel('Year', fontsize=18)
+        plt.show()
 
 
+BTC = cryptoVisual('BTC', 'USD', all_data=True)
+BTC.plotGraph()
 
+ETC = cryptoVisual('ETC', 'USD', all_data=True)
+ETC.plotGraph()
 
+LTC = cryptoVisual('LTC', 'USD', all_data=True)
+LTC.plotGraph()
 
-data = daily_price_historical('BTC', 'USD', all_data=True)
-# print (data.high, data.timestamp)
-plt.plot(data.timestamp, data.high)
-plt.title('BTC To USD')
-plt.ylabel('Price In USD')
-plt.xlabel('Year')
-plt.xticks(rotation=45)
-plt.show()
