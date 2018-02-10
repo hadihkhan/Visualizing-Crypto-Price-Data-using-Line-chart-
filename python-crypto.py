7e11f245e0a04314a62e4224039fadae789375d4
Hadi Hussain Khan <hadihussainkhan@Hadis-MacBook-Pro.local>
2018-02-11 02:31:23 +0500

    Assignment 1 - OOP implementation for Any Coin with any timeframe

--- python-crypto.py
+++ python-crypto.py
@@ -5,15 +5,16 @@ import matplotlib.pyplot as plt
 
 
 class cryptoVisual:
-    def __init__(self, sym, comp_sym, all_data=False, limit=1000, aggregate=1, exchange=''):
+    def __init__(self, sym, comp_sym, all_data=False, limit=1000, aggregate=1, exchange='', timeframe='histominute'):
+        self.timeframe = timeframe
         self.symbol = sym
         self.comp_sym = comp_sym
         self.all_data = all_data
         self.limit = limit
         self.aggregate = aggregate
         self.exchange = exchange
-        self.url = 'https://min-api.cryptocompare.com/data/histoday?fsym={}&tsym={}&limit={}&aggregate={}' \
-            .format(self.symbol.upper(), self.comp_sym.upper(), self.limit, self.aggregate)
+        self.url = 'https://min-api.cryptocompare.com/data/{}?fsym={}&tsym={}&limit={}&aggregate={}' \
+            .format(self.timeframe, self.symbol.upper(), self.comp_sym.upper(), self.limit, self.aggregate)
         if self.exchange:
             self.url += '&e={}'.format(self.exchange)
         if self.all_data:
@@ -30,13 +31,16 @@ class cryptoVisual:
         data = self.getData()
         fg = plt.figure(figsize=(20, 10))
         plt.plot(data.timestamp, data.high)
-        plt.title(self.symbol + ' To ' + self.comp_sym, fontsize=24)
+        plt.title(self.symbol + ' To ' + self.comp_sym + ' Time Frequency = ' + self.timeframe, fontsize=24)
         plt.ylabel('Price In ' + self.comp_sym, fontsize=18)
         plt.xlabel('Year', fontsize=18)
         plt.show()
 
 
-BTC = cryptoVisual('BTC', 'USD', all_data=True)
+BTC = cryptoVisual('BTC', 'PKR', all_data=True)
+BTC.plotGraph()
+
+BTC = cryptoVisual('BTC', 'USD', all_data=True, timeframe='histohour')
 BTC.plotGraph()
 
 ETC = cryptoVisual('ETC', 'USD', all_data=True)
@@ -44,4 +48,3 @@ ETC.plotGraph()
 
 LTC = cryptoVisual('LTC', 'USD', all_data=True)
 LTC.plotGraph()
-
